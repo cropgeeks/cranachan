9c0fab22cf4e81b758bc120b9fe49851caa9ba6f
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2017-11-13 13:13:08 +0000

    - CHG: Updated database schema template. created_on and updated_on now both use CURRENT_TIMESTAMP as the default value. - NEW: Added .gitignore file to make sure no files that shouldn't be comitted are committed. This file will evolve over time as requirements change.

--- docs/database/cranachan.sql
+++ docs/database/cranachan.sql
@@ -12,16 +12,12 @@ CREATE TABLE `datasets` (
   `filepath` text,
   `description` json DEFAULT NULL,
   `refseqset_id` int(11) DEFAULT NULL,
-  `created_on` datetime DEFAULT NULL,
-  `updated_on` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_on` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `datasets_ibfk_1` (`refseqset_id`) USING BTREE,
   CONSTRAINT `datasets_ibfk_1` FOREIGN KEY (`refseqset_id`) REFERENCES `refseqsets` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of datasets
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for genes
@@ -32,16 +28,12 @@ CREATE TABLE `genes` (
   `doi` text,
   `name` varchar(255) NOT NULL,
   `geneset_id` int(11) DEFAULT NULL,
-  `created_on` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `updated_on` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_on` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `geneset_id` (`geneset_id`) USING BTREE,
   CONSTRAINT `genes_ibfk_1` FOREIGN KEY (`geneset_id`) REFERENCES `genesets` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of genes
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for genes_to_refseqs
@@ -51,18 +43,14 @@ CREATE TABLE `genes_to_refseqs` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `gene_id` int(11) NOT NULL,
   `refseq_id` int(11) NOT NULL,
-  `created_on` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `updated_on` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_on` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `gene_id` (`gene_id`) USING BTREE,
   KEY `refseq_id` (`refseq_id`) USING BTREE,
   CONSTRAINT `genes_to_refseqs_ibfk_1` FOREIGN KEY (`gene_id`) REFERENCES `genes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `genes_to_refseqs_ibfk_2` FOREIGN KEY (`refseq_id`) REFERENCES `refseqs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of genes_to_refseqs
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for genesets
@@ -73,14 +61,10 @@ CREATE TABLE `genesets` (
   `doi` text,
   `name` varchar(255) NOT NULL,
   `description` json DEFAULT NULL,
-  `created_on` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `updated_on` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_on` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of genesets
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for refseqs
@@ -93,16 +77,12 @@ CREATE TABLE `refseqs` (
   `refseqset_id` int(11) NOT NULL,
   `length` bigint(20) DEFAULT NULL,
   `type` enum('UNKNOWN') DEFAULT 'UNKNOWN',
-  `created_on` datetime DEFAULT NULL,
-  `updated_on` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_on` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `refseqset_id` (`refseqset_id`) USING BTREE,
   CONSTRAINT `refseqs_ibfk_1` FOREIGN KEY (`refseqset_id`) REFERENCES `refseqsets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of refseqs
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for refseqsets
@@ -113,14 +93,10 @@ CREATE TABLE `refseqsets` (
   `doi` text,
   `name` varchar(255) NOT NULL,
   `description` json DEFAULT NULL,
-  `created_on` datetime DEFAULT NULL,
-  `updated_on` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_on` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of refseqsets
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for samples
@@ -130,14 +106,10 @@ CREATE TABLE `samples` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `doi` text,
   `name` varchar(255) NOT NULL,
-  `created_on` datetime DEFAULT NULL,
-  `updated_on` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_on` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of samples
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for samples_to_datasets
@@ -148,16 +120,12 @@ CREATE TABLE `samples_to_datasets` (
   `doi` text,
   `sample_id` int(11) NOT NULL,
   `dataset_id` int(11) NOT NULL,
-  `created_on` datetime DEFAULT NULL,
-  `updated_on` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_on` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_on` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `germplasm_id` (`sample_id`) USING BTREE,
   KEY `dataset_id` (`dataset_id`) USING BTREE,
   CONSTRAINT `samples_to_datasets_ibfk_1` FOREIGN KEY (`sample_id`) REFERENCES `samples` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `samples_to_datasets_ibfk_2` FOREIGN KEY (`dataset_id`) REFERENCES `datasets` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Records of samples_to_datasets
--- ----------------------------
 SET FOREIGN_KEY_CHECKS=1;
