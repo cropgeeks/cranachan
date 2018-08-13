e149588cb03e72c1a5c730bea2c9c5cb1a4cdcd1
gordonstephen <12711372+gordonstephen@users.noreply.github.com>
2018-08-13 11:21:31 +0100

    - NEW: We now produce Flapjack project files as part of the by-positions search.

---
+++ src/jhi/cranachan/bcftools/FlapjackCreateProject.java
@@ -0,0 +1,77 @@
+package jhi.cranachan.bcftools;
+
+import java.io.*;
+import java.util.*;
+import java.util.logging.*;
+
+public class FlapjackCreateProject
+{
+	private static Logger LOG = Logger.getLogger(FlapjackCreateProject.class.getName());
+
+	private File genotypeFile;
+	private File mapFile;
+	private File projectFile;
+
+	public FlapjackCreateProject(File genotypeFile, File projectFile)
+	{
+		this.genotypeFile = genotypeFile;
+		this.projectFile = projectFile;
+	}
+
+	public FlapjackCreateProject withMapFile(File mapFile)
+	{
+		this.mapFile = mapFile;
+
+		return this;
+	}
+
+	public void run(String flapjackPath, String tmpDir)
+		throws Exception
+	{
+		List<String> args = new ArrayList<>();
+
+		args.add("java");
+		args.add("-cp");
+		args.add(flapjackPath);
+		args.add("jhi.flapjack.io.cmd.CreateProject");
+		if (genotypeFile != null && projectFile != null)
+		{
+			args.add("-g");
+			args.add(genotypeFile.getPath());
+			args.add("-p");
+			args.add(projectFile.getPath());
+		}
+		if (mapFile != null)
+		{
+			args.add("-m");
+			args.add(mapFile.getPath());
+		}
+
+		ProcessBuilder pb = new ProcessBuilder(args);
+
+		System.out.println();
+		for (String command: pb.command())
+			System.out.print(command + " ");
+		System.out.println();
+
+		for (String command: pb.command())
+			LOG.log(Level.INFO, command);
+
+		pb.directory(new File(tmpDir));
+		pb.redirectErrorStream(true);
+
+		Process proc = pb.start();
+		LOG.log(Level.INFO, "Process started");
+
+		// Open up the input stream (to read from) (prog's out stream)
+		StreamCatcher oStream = new StreamCatcherLogger(proc.getInputStream());
+
+		proc.waitFor();
+		LOG.log(Level.INFO, "Waiting for process...");
+
+		while (oStream.isAlive())
+			Thread.sleep(500);
+
+		LOG.log(Level.INFO, "Process completed");
+	}
+}
--- src/jhi/cranachan/data/ResultsManagedBean.java
+++ src/jhi/cranachan/data/ResultsManagedBean.java
@@ -10,6 +10,10 @@ import javax.inject.*;
 import jhi.cranachan.bcftools.*;
 import jhi.cranachan.database.*;
 
+import jhi.flapjack.io.cmd.*;
+
+import org.primefaces.model.*;
+
 @Named
 @ViewScoped
 public class ResultsManagedBean implements Serializable
@@ -26,6 +30,13 @@ public class ResultsManagedBean implements Serializable
 	// Location for storing files created by this class
 	private String tmpDir;
 
+	private List<File> mapFiles = new ArrayList<>();
+	private List<File> genotypeFiles = new ArrayList<>();
+	private File projectFile;
+
+	private boolean projectFileCreated = false;
+	private boolean byPosition = false;
+
 	public ResultsManagedBean()
 	{
 	}
@@ -41,7 +52,10 @@ public class ResultsManagedBean implements Serializable
 
 		// We either create rows for our results table by calling code which parses parameters from search-by-position.xhtml
 		if (requestParams.containsKey("by-position"))
+		{
 			createByPositionRows(requestParams);
+			byPosition = true;
+		}
 
 		// Or we create rows for our results table by calling code which parses parameters from search-by-gene.xhtml
 		else if (requestParams.containsKey("by-gene"))
@@ -63,11 +77,46 @@ public class ResultsManagedBean implements Serializable
 		String name = chromosome + "-" + start + "-" + end;
 
 		// Run bcftools and generate the file
-		File file = runBcfTools(name, dataset, chromosome, start, end);
+		File file = runBcfToolsView(name, dataset, chromosome, start, end);
+
+		File stats = runBcfToolsStats(file.getName(), file);
+		long snpCount = getSnpCount(stats);
 
 		// Add the results to the list which we will use in the JSF page to generate the table
-		GeneTableRow row = new GeneTableRow("", chromosome, start, end, 0, file);
+		GeneTableRow row = new GeneTableRow("", chromosome, start, end, snpCount, file);
 		tableRows.add(row);
+
+		if (snpCount > 0)
+		{
+			projectFileCreated = true;
+
+			String mapFileName = changeFileExtension(file.getName(), ".map");
+			File mapFile = new File(tmpDir, mapFileName);
+			mapFiles.add(mapFile);
+
+			String genotypeFileName = changeFileExtension(file.getName(), ".dat");
+			File genotypeFile = new File(tmpDir, genotypeFileName);
+			genotypeFiles.add(genotypeFile);
+
+			VcfToFJTabbedConverter vcfConverter = new VcfToFJTabbedConverter(file, mapFile, genotypeFile);
+			vcfConverter.convert();
+
+			String projectFileName = changeFileExtension(file.getName(), ".flapjack");
+			projectFile = new File("/tmp", projectFileName);
+
+			String flapjackPath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/WEB-INF/lib/flapjack.jar");
+
+			try
+			{
+				FlapjackCreateProject createProject = new FlapjackCreateProject(genotypeFile, projectFile)
+					.withMapFile(mapFile);
+				createProject.run(flapjackPath, tmpDir);
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
 	}
 
 	private void createByGeneRows(Map<String, String> requestParams)
@@ -94,7 +143,7 @@ public class ResultsManagedBean implements Serializable
 			// in the JSF page to generate the table
 			for(Gene gene: genes)
 			{
-				File file = runBcfTools(gene.getName(), dataset, gene.getChromosome(), gene.getStart()-extendRegion, gene.getEnd()+extendRegion);
+				File file = runBcfToolsView(gene.getName(), dataset, gene.getChromosome(), gene.getStart()-extendRegion, gene.getEnd()+extendRegion);
 
 				GeneTableRow row = new GeneTableRow(gene.getName(), gene.getChromosome(), gene.getStart(), gene.getEnd(), 0, file);
 				tableRows.add(row);
@@ -102,7 +151,7 @@ public class ResultsManagedBean implements Serializable
 		}
 	}
 
-	private File runBcfTools(String name, Dataset dataset, String chromosomeName, long start, long end)
+	private File runBcfToolsView(String name, Dataset dataset, String chromosomeName, long start, long end)
 	{
 		File output = new File(tmpDir, name + "-" + System.currentTimeMillis() + ".vcf");
 		try
@@ -123,9 +172,89 @@ public class ResultsManagedBean implements Serializable
 		return output;
 	}
 
+	private File runBcfToolsStats(String name, File vcfFile)
+	{
+		File output = new File(tmpDir, name + ".stats");
+		try
+		{
+			BcfToolsStats stats = new BcfToolsStats(vcfFile)
+				.withOutputFile(output);
+
+			stats.run("BCFTOOLS", tmpDir);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		return output;
+	}
+
+	private long getSnpCount(File file)
+	{
+		long snpCount = 0;
+
+		try (BufferedReader reader = new BufferedReader(new FileReader(file)))
+		{
+			String line = "";
+			while ((line = reader.readLine()) != null)
+			{
+				if (line.startsWith("SN"))
+				{
+					String[] tokens = line.split("\t");
+					System.out.println(String.join(",", tokens));
+					if (tokens[2].equalsIgnoreCase("number of snps:"))
+					{
+						snpCount = Integer.parseInt(tokens[3]);
+						return snpCount;
+					}
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		return snpCount;
+	}
+
+	private String changeFileExtension(String filename, String extension)
+	{
+		String newFileName = filename;
+		if (newFileName.indexOf(".") > 0)
+			newFileName = newFileName.substring(0, newFileName.lastIndexOf(".")) + extension;
+
+		return newFileName;
+	}
+
+	public DefaultStreamedContent downloadProject()
+		throws Exception
+	{
+		return new DefaultStreamedContent(new FileInputStream(projectFile), "application/octet-stream", projectFile.getName());
+	}
+
+	public File getProjectFile()
+		{ return projectFile; }
+
+	public void setProjectFile(File projectFile)
+		{ this.projectFile = projectFile; }
+
 	public ArrayList<GeneTableRow> getTableRows()
 		{ return tableRows; }
 
 	public void setTableRows(ArrayList<GeneTableRow> tableRows)
 		{ this.tableRows = tableRows; }
+
+	public boolean isProjectFileCreated()
+		{ return projectFileCreated; }
+
+	public void setProjectFileCreated(boolean projectFileCreated)
+		{ this.projectFileCreated = projectFileCreated; }
+
+	public boolean isByPosition()
+		{ return byPosition; }
+
+	public void setByPosition(boolean byPosition)
+		{ this.byPosition = byPosition; }
 }
